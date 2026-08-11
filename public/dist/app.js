79b338d0f0c275b81531f7f8073efaed3e174ca5
Maykon henrique <henriquemaykon418@gmail.com>
2026-08-11 18:32:26 -0300

    Update PHONE constant value

--- public/dist/app.js
+++ public/dist/app.js
@@ -1,5 +1,5 @@
 "use strict";
-const PHONE = "5561992927072";
+const PHONE = "556192927072";
 const money = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" });
 const app = document.querySelector("#app");
 let cart = [];
